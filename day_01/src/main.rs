18fec9bd89e850a6bc391f656f5df0c59d907004
sbock <sebastian.bockholt@posteo.de>
2020-12-01 22:56:50 +0100

    fix refactor duplicate code

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -11,26 +11,25 @@ fn compute(numbers: Vec<usize>, number_count: usize) -> Option<Vec<usize>> {
     None
 }
 
+fn generate_output(numbers: Vec<usize>, number_count: usize) {
+    match compute(numbers.clone(), number_count) {
+        Some(arr) => {
+            let formatting = arr.iter().join("*");
+            let result = arr.iter().fold(1, |acc, x| acc * x);
+            println!("{} = {}", formatting, result);
+        },
+        None => println!("Did not find anything /o\\")
+    }
+}
+
 fn main() {
     let input_str = fs::read_to_string("./src/input.txt")
         .expect("Something went wrong reading the file...");
     let numbers: Vec<usize> = input_str.split("\n")
         .flat_map(|number_str| number_str.parse::<usize>())
         .collect();
-    match compute(numbers.clone(), 2) {
-        Some(arr) => {
-            let formatting = arr.iter().join("*");
-            let result = arr.iter().fold(1, |acc, x| acc * x);
-            println!("{} = {}", formatting, result);
-        },
-        None => println!("Did not find anything /o\\")
-    }
-    match compute(numbers, 3) {
-        Some(arr) => {
-            let formatting = arr.iter().join("*");
-            let result = arr.iter().fold(1, |acc, x| acc * x);
-            println!("{} = {}", formatting, result);
-        },
-        None => println!("Did not find anything /o\\")
-    }
+    print!("Stage 1: ");
+    generate_output(numbers.clone(), 2);
+    print!("Stage 2: ");
+    generate_output(numbers, 3);
 }
