a5552eff56e5cd8d83c7d19ff69dd80fdefcab21
sbock <sebastian.bockholt@posteo.de>
2020-12-15 09:16:48 +0100

    add second stage gauss trick

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -36,6 +36,10 @@ fn boarding_pass_to_seat(boarding_pass: String) -> Seat {
     return Seat { row, column };
 }
 
+fn gauss_sum(n: usize) -> usize {
+    (n.pow(2) + n)/2
+}
+
 fn main() {
     let input_str =
         fs::read_to_string("./src/input.txt").expect("Something went wrong reading the file...");
@@ -43,9 +47,33 @@ fn main() {
         .split_whitespace()
         .map(String::from)
         .collect::<Vec<String>>();
-    let maximum = boarding_passes
+    let seat_id = boarding_passes
         .iter()
         .map(|pass| boarding_pass_to_seat(pass.to_string()))
-        .map(|pass| pass.seat_id()).max().unwrap();
-    println!("The maximum seat id is {}", maximum);
+        .map(|pass| pass.seat_id());
+    let max = seat_id.clone().max().unwrap();
+    let min = seat_id.clone().min().unwrap();
+    let missing_id = (gauss_sum(max) - gauss_sum(min-1)) - seat_id.sum::<usize>();
+    println!("The maximum seat id is {}", max);
+    println!("The missing seat id is {}", missing_id);
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_gauss() -> Result<(), String> {
+        let n: usize = 4;
+        assert_eq!(gauss_sum(n), (0..=n).sum());
+        Ok(())
+    }
+
+    #[test]
+    fn test_subtraction() -> Result<(), String> {
+        assert_eq!(gauss_sum(10) - gauss_sum(5-1), (5..=10).sum());
+        Ok(())
+    }
+}
+
